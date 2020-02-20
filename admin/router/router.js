29f72fea5648a0bd86cdc9a7f107ecd4326bad63
nikhn <nicolas.pattyn@etu.unilim.fr>
2020-02-20 10:34:13 +0100

    Projet

---
+++ admin/router/router.js
@@ -0,0 +1,18 @@
+
+let HomeController = require('./../controllers/HomeController');
+
+
+// Routes
+module.exports = function(app){
+
+// Main Routes
+    app.get('/', HomeController.Index);
+    app.get('/accueil', HomeController.Index);
+
+
+
+// tout le reste
+app.get('*', HomeController.NotFound);
+app.post('*', HomeController.NotFound);
+
+};
