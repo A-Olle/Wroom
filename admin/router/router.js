b96511028e32bd12fc9df7ed46504513b0dbda05
nikhn <nicolas.pattyn@etu.unilim.fr>
2020-02-22 09:32:41 +0100

    Ajout blocage des pages si non connecté

--- admin/router/router.js
+++ admin/router/router.js
@@ -1,18 +1,25 @@
 
 let HomeController = require('./../controllers/HomeController');
-let ConnexionController = require('./../controllers/ConnexionController');
+let PiloteController = require('./../controllers/PiloteController');
+let AuthentificationController = require('./../controllers/AuthentificationController');
+
 
 
 // Routes
 module.exports = function(app){
 
 // Main Routes
+
     app.get('/', HomeController.Index);
     app.get('/accueil', HomeController.Index);
     app.post('/accueil', HomeController.Connexion);
     app.post('/', HomeController.Connexion);
 
-
+//Pilote
+    app.get('/pilotes',AuthentificationController.VerifierConnecter);
+    app.get('/pilotes',PiloteController.Repertoire);
+    app.get('/ajouterPilote',PiloteController.Pays);
+  
 
 
 // tout le reste
